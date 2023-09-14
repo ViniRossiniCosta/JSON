cd7569a6767eb7ad7543b4b61051fabe95e56421
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-14 09:49:32 -0300

    JSON

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const obj = [
+    {
+        "nome": "Vinicius",
+        "idade": 30,
+        "esta_trabalhando": true,
+        "detalhes_profissao": {
+            "nome_profissao": "Programador",
+            "empresa_atual": "Freeline info.",
+            "horas_semanais": 30
+        },
+        "hobbies": [
+            "Programar",
+            "jogar piant-ball",
+            "jogar CS 1.6",
+            "Maratonar Black-Mirror"
+        ],
+    },
+]
